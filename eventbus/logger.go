7ec78cff400af95e1594a96f10dbd6b12c1d35d9
qiulin <liqiulin@outlook.com>
2023-07-16 02:29:43 +0800

    update: eventbus

---
+++ eventbus/logger.go
@@ -0,0 +1,60 @@
+package eventbus
+
+import (
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/fluxstack/fluxworks/log"
+)
+
+type Logger struct {
+	logger *log.Logger
+}
+
+func NewLogger(logger *log.Logger) *Logger {
+	return &Logger{logger: logger}
+}
+
+func (l *Logger) Error(msg string, err error, fields watermill.LogFields) {
+	f := log.Fields{}
+	f[log.DefaultMessageKey] = msg
+	f["error"] = err.Error()
+	for k, v := range fields {
+		f[k] = v
+	}
+	l.logger.Errorw(f)
+}
+
+func (l *Logger) Info(msg string, fields watermill.LogFields) {
+	f := log.Fields{}
+	f[log.DefaultMessageKey] = msg
+	for k, v := range fields {
+		f[k] = v
+	}
+	l.logger.Infow(f)
+}
+
+func (l *Logger) Debug(msg string, fields watermill.LogFields) {
+	f := log.Fields{}
+	f[log.DefaultMessageKey] = msg
+	for k, v := range fields {
+		f[k] = v
+	}
+	l.logger.Debugw(f)
+}
+
+func (l *Logger) Trace(msg string, fields watermill.LogFields) {
+	f := log.Fields{}
+	f[log.DefaultMessageKey] = msg
+	for k, v := range fields {
+		f[k] = v
+	}
+	l.logger.Debugw(f)
+}
+
+func (l *Logger) With(fields watermill.LogFields) watermill.LoggerAdapter {
+	f := log.Fields{}
+	for k, v := range fields {
+		f[k] = v
+	}
+	logger := l.logger.With(f)
+	return &Logger{logger: logger}
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module github.com/fluxstack/fluxworks
 go 1.20
 
 require (
+	github.com/ThreeDotsLabs/watermill v1.3.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/lithammer/shortuuid/v3 v3.0.7 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/samber/lo v1.38.1 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
